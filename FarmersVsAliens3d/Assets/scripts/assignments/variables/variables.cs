c99f362f78ed4609f24ed61c4f912c9d055abc82
riley <10744151@my.uvu.edu>
2020-01-15 17:17:35 -0700

    edited my variable script

---
+++ FarmersVsAliens3d/Assets/scripts/assignments/variables/variables.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour
+{// Declaration
+    public int cookieBox;
+    public float waterBottle;
+    private string firstName;
+    public double rbi;
+    public GameObject player;
+    public float amount;
+    public float money;
+    public float cost;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        Cookies(amount, money, cost);
+
+
+
+        cookieBox = 20;
+        firstName = "Bobby";
+        Debug.Log("How many cookies are in the box " + cookieBox);
+        //Function call
+        CheckScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Cookies(float amount, float, money, float, cost)
+    {
+        float total;
+
+        total = money - cost * amount;
+
+        print(total);
+    }
+
+    void CheckScore()
+    {
+        print("Hello World!");
+    }
+
+}
