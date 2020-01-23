7cca36fb4d9fd36a12a9dac008e357c544a872a4
riley <10744151@my.uvu.edu>
2020-01-22 17:23:05 -0700

    class 1-22

---
+++ FarmersVsAliens3d/Assets/scripts/assignments/variables/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, 0.1f);
+        
+    }
+
+    // detect collision with another object
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
---
+++ FarmersVsAliens3d/Assets/scripts/assignments/variables/test.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{// Declaration
+    public int cookieBox;
+    public float waterBottle;
+    private string firstName;
+    public double rbi;
+    public GameObject player;
+    public float heart;
+    public float feelgood;
+    public float beans;
+    public float farts;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+
+        Cookies(farts, feelgood, beans);
+
+
+        cookieBox = 20;
+        firstName = "Bobby";
+        Debug.Log("How many cookies are in the box " + cookieBox);
+        //Function call
+        CheckScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Heart(float beans, float heart, float feelgood)
+    {
+        heart = beans + feelgood;
+
+    }
+
+    void Cookies(float beans, float feelgood, float farts)
+    {
+
+        feelgood = beans * farts;
+
+
+        print(feelgood);
+    }
+
+    void CheckScore()
+    {
+        print("Hello World!");
+    }
+}
