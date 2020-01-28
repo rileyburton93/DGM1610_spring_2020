ac6c27e6ccdbf430dc2a5af52848a4b85279081f
riley <10744151@my.uvu.edu>
2020-01-27 17:17:17 -0700

    class 1-27

--- FarmersVsAliens3d/Assets/scripts/assignments/variables/Movement.cs
+++ FarmersVsAliens3d/Assets/scripts/assignments/variables/Movement.cs
@@ -20,11 +20,23 @@ public class Movement : MonoBehaviour
     // detect collision with another object
     private void OnCollisionEnter(Collision other)
     {
-        
+
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            Debug.Log("Colliding with Floor");
+        }
+        else if(other.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Colliding with Obstacle");
+        }
+        else
+        {
+            Debug.Log("...");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log("You have entered the trigger!");
     }
 }
