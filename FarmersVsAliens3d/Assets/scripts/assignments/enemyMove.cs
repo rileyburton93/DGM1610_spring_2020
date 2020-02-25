86021a8035c3ca2b8d635213b016addfba8226d3
m <m>
2020-02-24 17:20:12 -0700

    enemymove

--- FarmersVsAliens3d/Assets/scripts/assignments/enemyMove.cs
+++ FarmersVsAliens3d/Assets/scripts/assignments/enemyMove.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class enemyMove : MonoBehaviour
 {
-    public Transform target;
+    
+    public GameObject player;
+    public Rigidbody enemyRb;
     public float moveSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+       // transform.LookAt(player);
+       // transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+    void FixedUpdate()
+    {
+
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * moveSpeed);
+
     }
 }
