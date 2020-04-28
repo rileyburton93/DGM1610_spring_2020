9637771a7ef862891c8973c7da8947067924b407
d <d>
2020-04-27 23:50:12 -0600

    A3 commits

---
+++ CommitsProject/Assets/Scripts/A3Scripts/A3Script1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A3Script1 : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody enemyRb;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player Cube");
+    }
+
+    void FixedUpdate()
+    {
+
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * moveSpeed);
+
+    }
+
+}
---
+++ CommitsProject/Assets/Scripts/A3Scripts/A3Script2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A3Script2 : MonoBehaviour
+{
+
+    public int enemyHP;
+    public int maxHealth = 1;
+    public Transform spawnPoint;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyHP = maxHealth;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Projectile"))
+            {
+            enemyHP = enemyHP - 1;
+            print("Enemy is Injured");
+            }
+        
+    }
+
+    
+
+    public void TakeHP(int amount)
+    {
+        enemyHP -= amount;
+        if (enemyHP <= 0)
+        {
+            enemyHP = 0; 
+            transform.position = spawnPoint.position;
+            transform.rotation = spawnPoint.rotation;
+            enemyHP = maxHealth;
+        }
+
+    }
+}
